2e2639a8d9c9b9b6bc62863ae3890c9513eb106a
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-15 00:45:43 -0400

    Fixing formatting

--- src/cljs/chromatophore/toggle.cljs
+++ src/cljs/chromatophore/toggle.cljs
@@ -8,17 +8,19 @@
              :refer [atom]
              :refer-macros [run!]]))
 
-(defnp unfold-reveal
+(defnp
+  unfold-reveal
   "`unfold-reveal` is a component with sub components you can toggle the visibility of by clicking a symbol"
   [{:keys [folded-arrow unfolded-arrow unfolded? on-toggle override on-override]
-    :or {unfolded? false
-         folded-arrow "▶︎"
-         unfolded-arrow "▼"}}]
+    :or   {unfolded?      false
+           folded-arrow   "▶︎"
+           unfolded-arrow "▼"}}]
   (assert (boolean? unfolded?)
           "Initially unfolded-state parameter must be a boolean")
   (assert (or (nil? override)
               (and (ratom? override)
-                   (or (nil? @override) (boolean? @override))))
+                   (or (nil? @override)
+                       (boolean? @override))))
           "If override is specified, it must be a ratom containing either nil or a boolean")
   (let [unfolded-state (reagent/atom unfolded?)]
     (fnp unfold-reveal-fnp [parameters main & children]
@@ -29,13 +31,11 @@
 
          [:div.unfold-reveal
           parameters
-          ;; TODO: compute minimal width to span both unfolded-arrow
-          ;;       and folded-arrow using invisible divs or something
           [:span.toggle
            (merge parameters
-                  {:class (str "unselectable "
-                               (when (empty? children)
-                                 " deactivated"))
+                  {:class    (str "unselectable "
+                                  (when (empty? children)
+                                    " deactivated"))
                    :on-click #(do (swap! unfolded-state not)
                                   (if (fn? on-toggle)
                                     (on-toggle % @unfolded-state)))})
@@ -50,8 +50,8 @@
                    (if-not @unfolded-state {:style {:display "none"}}))
             ;; TODO: better to use a child-idx function than this
             (map-indexed
-             (fn [idx child] ^{:key idx} [:div.child parameters child])
-             children)]]])))
+              (fn [idx child] ^{:key idx} [:div.child parameters child])
+              children)]]])))
 
 (defn toggle-tree
   "A component for displaying a hierarchical tree of elements using `unfold-reveal` components"
@@ -66,14 +66,14 @@
          :else
          (ex-info "Cannot handle argument"
                   {:parameters nil
-                   :argument argument})))
+                   :argument   argument})))
 
   ([{:keys [on-toggle]
-     :as parameters}
+     :as   parameters}
     {:keys [main children unfolded?]
-     :or {children  []
-          unfolded? false}
-     :as argument}]
+     :or   {children  []
+            unfolded? false}
+     :as   argument}]
    (cond (or (string? argument)
              (vector? argument))
          [unfold-reveal parameters argument]
@@ -82,22 +82,22 @@
          (let [override (reagent/atom nil)
                ;; Reactive witch-craft to get `shift-click`
                ;; to unfold-all and collapse-all
-               on-override           #(do (reset! override nil)
-                                          (reset! override %))
-               new-on-toggle         #(do (when (.-shiftKey %1)
-                                            (on-override %2))
-                                          (when (fn? on-toggle)
-                                            (on-toggle %1 %2)))
-               child-parameters      (assoc parameters
-                                            :override override)
-               main-parameters       (merge argument
-                                            parameters
-                                            {:on-toggle new-on-toggle
-                                             :on-override on-override})]
+               on-override #(do (reset! override nil)
+                                (reset! override %))
+               new-on-toggle #(do (when (.-shiftKey %1)
+                                    (on-override %2))
+                                  (when (fn? on-toggle)
+                                    (on-toggle %1 %2)))
+               child-parameters (assoc parameters
+                                  :override override)
+               main-parameters (merge argument
+                                      parameters
+                                      {:on-toggle   new-on-toggle
+                                       :on-override on-override})]
            (apply vector unfold-reveal main-parameters main
                   (map (partial toggle-tree child-parameters) children)))
 
          :else
          (ex-info "Cannot handle argument"
                   {:parameters parameters
-                   :argument argument}))))
+                   :argument   argument}))))
