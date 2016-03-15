4af8781db513114c07994c94912ac025748eb139
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-15 00:45:30 -0400

    Fixing formatting

--- project.clj
+++ project.clj
@@ -1,54 +1,50 @@
 (defproject chromatophore "0.1.0-SNAPSHOT"
-  :description       "FIXME"
-  :url               "FIXME"
+  :description "FIXME"
+  :url "FIXME"
 
-  :source-paths      ["src/clj" "src/cljc"]
-  :test-paths        ["test/clj" "test/cljc"]
+  :source-paths ["src/clj" "src/cljc"]
+  :test-paths ["test/cljc"]
 
-  :dependencies [[cljsjs/react                         "0.14.3-0"]
-                 [cljsjs/react-dom                     "0.14.3-1"]
-                 [cljsjs/react-dom-server              "0.14.3-0"]
-                 [compojure                            "1.4.0"]
-                 [garden                               "1.3.0"]
-                 [hiccup                               "1.0.5"]
-                 [markdown-clj                         "0.9.85"]
-                 [org.clojure/clojure                  "1.8.0"]
-                 [org.clojure/clojurescript            "1.7.228"]
-                 [prismatic/schema                     "1.0.4"]
-                 [reagent                              "0.5.1"
-                  :exclusions [cljsjs/react-dom]]]
+  :dependencies [[compojure "1.4.0"]
+                 [garden "1.3.0"]
+                 [hiccup "1.0.5"]
+                 [markdown-clj "0.9.85"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [prismatic/schema "1.0.4"]
+                 [reagent "0.6.0-alpha"]]
 
-  :profiles {:dev   {:plugins      [[com.jakemccrary/lein-test-refresh "0.12.0"]
-                                    [lein-cljsbuild  "1.1.2"
-                                     :exclusions
-                                     [[org.apache.commons/commons-compress]
-                                      [org.clojure/clojure]]]
-                                    [org.apache.commons/commons-compress "1.4"]
-                                    [lein-npm        "0.6.2"]
-                                    [lein-figwheel   "0.5.0-6"]
-                                    [lein-garden     "0.2.6"]
-                                    [lein-doo        "0.1.6"]
-                                    [lein-pdo        "0.1.1"]
-                                    [lein-shell      "0.5.0"]]
-                     :dependencies [[devcards                  "0.2.1-6"]
-                                    [javax.servlet/servlet-api "2.5"]]}}
+  :profiles {:dev {:plugins      [[com.jakemccrary/lein-test-refresh "0.12.0"]
+                                  [lein-cljsbuild "1.1.2"
+                                   :exclusions
+                                   [[org.apache.commons/commons-compress]
+                                    [org.clojure/clojure]]]
+                                  [org.apache.commons/commons-compress "1.4"]
+                                  [lein-npm "0.6.2"]
+                                  [lein-figwheel "0.5.0-6"]
+                                  [lein-garden "0.2.6"]
+                                  [lein-doo "0.1.6"]
+                                  [lein-pdo "0.1.1"]
+                                  [lein-shell "0.5.0"]]
+                   :dependencies [[devcards "0.2.1-6"]
+                                  [javax.servlet/servlet-api "2.5"]]}}
 
   :garden {:builds
            [{:id           "screen"
              :source-paths ["src/clj"]
-             :stylesheet chromatophore.css.screen/style
-             :compiler {:output-to "resources/public/css/compiled/screen.css"
-                        :pretty-print? true}}]}
+             :stylesheet   chromatophore.css.screen/style
+             :compiler     {:output-to     "resources/public/css/compiled/screen.css"
+                            :pretty-print? true}}]}
 
   ;; Use NPM to get slimerjs and phantomjs
-  :npm {:dependencies [[slimerjs   "0.9.6"
+  :npm {:dependencies [[slimerjs "0.9.6"
                         phantomjs2 "2.0.2"]]}
 
   :doo {:build "test"
         :paths {:slimer    "./node_modules/.bin/slimerjs"
                 :phantomjs "./node_modules/.bin/phantomjs"}
-        :alias {:browsers  [:chrome :firefox]
-                :all       [:browsers :headless]}}
+        :alias {:browsers [:chrome :firefox]
+                :all      [:browsers :headless]}}
 
   :clean-targets ^{:protect false} [:target-path
                                     "resources/public/js/compiled/"
@@ -56,54 +52,53 @@
                                     "out/"
                                     "dev-resources/"]
 
-  :cljsbuild {:builds [{:id "devcards"
+  :cljsbuild {:builds [{:id           "devcards"
                         :source-paths ["src/cljs" "src/cljc" "test/cljs" "test/cljc"]
-                        :figwheel {:devcards             true}
-                        :compiler {:main                 "chromatophore.devcards.core"
-                                   :asset-path           "js/compiled/devcards_out"
-                                   :output-to            "resources/public/js/compiled/chromatophore_devcards.js"
-                                   :output-dir           "resources/public/js/compiled/devcards_out"
-                                   :source-map-timestamp true}}
-                       {:id "test"
+                        :figwheel     {:devcards true}
+                        :compiler     {:main                 "chromatophore.devcards.core"
+                                       :asset-path           "js/compiled/devcards_out"
+                                       :output-to            "resources/public/js/compiled/chromatophore_devcards.js"
+                                       :output-dir           "resources/public/js/compiled/devcards_out"
+                                       :source-map-timestamp true}}
+                       {:id           "test"
                         :source-paths ["src/cljs" "src/cljc" "test/cljc" "test/cljs" "test/doo"]
-                        :compiler {:output-to     "target/js/compiled/testable.js"
-                                   :main          "chromatophore.doo.runner"
-                                   :optimizations :none}}
+                        :compiler     {:output-to     "target/js/compiled/testable.js"
+                                       :main          "chromatophore.doo.runner"
+                                       :optimizations :none}}
 
-                       {:id "test-advanced"
+                       {:id           "test-advanced"
                         :source-paths ["src/cljs" "src/cljc" "test/cljs" "test/cljc" "test/doo"]
-                        :compiler {:output-to       "target/js/compiled/testable.min.js"
-                                   :output-dir      "target/js/compiled"
-                                   :main            "chromatophore.doo.runner"
-                                   :verbose         true
-                                   :optimizations   :advanced
-                                   :pretty-print    false}}]}
+                        :compiler     {:output-to     "target/js/compiled/testable.min.js"
+                                       :main          "chromatophore.doo.runner"
+                                       :optimizations :advanced
+                                       :pretty-print  false}}]}
 
-  :figwheel {:css-dirs      ["resources/public/css"]}
+  :figwheel {:css-dirs ["resources/public/css"]}
 
-  :aliases {"garden-and-devcards"  ["pdo"
-                                    "garden" "auto,"
-                                    "test-refresh,"
-                                    "figwheel,"]
-            "devcards"             ["do"
-                                    "clean,"
-                                    "garden" "once,"
-                                    "garden-and-devcards,"]
-            "test-auto"            ["do"
-                                    "clean,"
-                                    "npm" "install,"
-                                    "doo" "phantom" "test" "auto,"]
-            "auto-test"            ["test-auto"]
-            "test-advanced"        ["do"
-                                    "clean,"
-                                    "npm" "install,"
-                                    "garden" "once,"
-                                    "test,"
-                                    ;;"doo" "all" "test" "once,"
-                                    "doo" "phantom" "test-advanced" "once,"]
-            "advanced-test"        ["test-advanced"]
-            "deep-clean"           ["do"
-                                    "shell" "rm" "-rf"
-                                    "figwheel_server.log"
-                                    "node_modules,"
-                                    "clean"]})
+  :aliases {"garden-and-devcards" ["pdo"
+                                   "garden" "auto,"
+                                   "test-refresh,"
+                                   "figwheel,"]
+            "devcards"            ["do"
+                                   "clean,"
+                                   "garden" "once,"
+                                   "test,"
+                                   "garden-and-devcards,"]
+            "test-auto"           ["do"
+                                   "clean,"
+                                   "npm" "install,"
+                                   "doo" "phantom" "test" "auto,"]
+            "auto-test"           ["test-auto"]
+            "test-advanced"       ["do"
+                                   "clean,"
+                                   "npm" "install,"
+                                   "garden" "once,"
+                                   "test,"
+                                   "doo" "all" "test" "once,"
+                                   "doo" "all" "test-advanced" "once,"]
+            "advanced-test"       ["test-advanced"]
+            "deep-clean"          ["do"
+                                   "shell" "rm" "-rf"
+                                   "figwheel_server.log"
+                                   "node_modules,"
+                                   "clean"]})
