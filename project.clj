b2f1916323b89ee12c0cef356722e6e1e0fabf6e
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-04-26 20:32:22 -0400

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chromatophore "0.1.5"
+(defproject chromatophore "0.1.6"
   :description "CuttleFi.sh Reusable Components for Reagent"
 
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
